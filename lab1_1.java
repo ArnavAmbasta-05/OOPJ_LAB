560d4abc2993dfb2c6e5baed982e2e24e5dad6ef
ArnavAmbasta-05 <iamarnavambasta@gmail.com>
2025-12-11 16:29:05 +0530

    lab1-1

---
+++ lab1_1.java
@@ -0,0 +1,8 @@
+public class lab1_1 {
+    public static void main (String args []){
+        System.out.println("Name : Arnav Ambasta");
+        System.out.println("Roll No. : 24051473");
+        System.out.println("Section: CSE-48");
+        System.out.println("Branch : Computer Science and Engineering");
+    }
+}
