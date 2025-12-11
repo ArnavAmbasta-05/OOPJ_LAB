57accbc21c531e4484b75d71ff230287e1b2e5e2
ArnavAmbasta-05 <iamarnavambasta@gmail.com>
2025-12-11 17:11:00 +0530

    lab1-5

---
+++ lab1_5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class lab1_5 {
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first name:");
+        String first_name = sc.next();
+        System.out.println("Enter the second name:");
+        String second_name = sc.next();
+        System.out.println(second_name + first_name);
+    }
+}
